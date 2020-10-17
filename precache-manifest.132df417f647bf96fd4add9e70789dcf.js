e73f42c6550eda5db68e6fb1054ccf502837d963
panyuanfeng <8017249+panyuanfeng@user.noreply.gitee.com>
2020-10-17 23:00:24 +0800

    deploy

---
+++ precache-manifest.132df417f647bf96fd4add9e70789dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295c23d39e12606ccf92",
+    "url": "/bookkeep-website/css/app.45038fa8.css"
+  },
+  {
+    "revision": "7e8246f9c57eff312230a04fa96b4e00",
+    "url": "/bookkeep-website/index.html"
+  },
+  {
+    "revision": "295c23d39e12606ccf92",
+    "url": "/bookkeep-website/js/app.f5074865.js"
+  },
+  {
+    "revision": "090bf66979f8c157a15d",
+    "url": "/bookkeep-website/js/chunk-vendors.bc9d833f.js"
+  },
+  {
+    "revision": "5b23cfa084ce86dc7c676499e6070c7e",
+    "url": "/bookkeep-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeep-website/robots.txt"
+  }
+]);
